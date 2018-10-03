eb6ef987c31384926e32237fcf67c8d0c8552c68
zhaoangus <zhao_angus@163.com>
2018-10-03 10:19:31 +0800

    animation finished

--- src/common/gallary/Gallary.vue
+++ src/common/gallary/Gallary.vue
@@ -59,7 +59,7 @@ export default {
     .wrapper
       width: 100%
       height: 0
-      padding-bottom: 50%
+      padding-bottom: 100%
       .gallary-img
         width: 100%
       .swiper-pagination
